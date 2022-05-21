7264ff568051291c1e2bcfbd58c11080740dd85f
ThunderSphun <manderijn2000@gmail.com>
2022-05-21 11:56:13 +0200

    template

--- main.cpp
+++ main.cpp
@@ -2,20 +2,8 @@
 #include "opencv2/highgui/highgui.hpp"
 
 int main() {
-	cv::namedWindow("base", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("resize", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("crop", cv::WINDOW_AUTOSIZE);
-
-	cv::Mat base = cv::imread("Images/bert en ernie.jpg", cv::IMREAD_COLOR);
-	cv::Mat resize = base.clone();
-	cv::Mat crop = base.clone();
-
-	resize.resize(200);
-
-	imshow("base", base);
-	imshow("resize", resize);
-	imshow("crop", crop);
 
 	cv::waitKey(0);
+	cv::destroyAllWindows();
 	return 0;
 }
