3c385583ee057d31fd02a8d703e47f7b72e30a9f
ThunderSphun <manderijn2000@gmail.com>
2022-05-21 16:17:11 +0200

    solution

--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@
 using namespace cv;
 
 int main() {
-	namedWindow("template", WINDOW_AUTOSIZE);
+	namedWindow("ball with stripes", WINDOW_AUTOSIZE);
+	namedWindow("ball without stripes", WINDOW_AUTOSIZE);
+
+	Mat stripes = imread("Images/ballstripes.bmp");
+	Mat stripeless;
+
+	dilate(stripes, stripeless, getStructuringElement(MORPH_RECT, Size(5, 5)));
+	erode(stripeless, stripeless, getStructuringElement(MORPH_RECT, Size(5, 5)));
+
+	imshow("ball with stripes", stripes);
+	imshow("ball without stripes", stripeless);
 
 	waitKey(0);
 	destroyAllWindows();
