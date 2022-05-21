d1c940c8465c9b70af38746f42a9bd8ac2d8e1e3
ThunderSphun <manderijn2000@gmail.com>
2022-05-21 11:54:19 +0200

    solution

--- main.cpp
+++ main.cpp
@@ -6,16 +6,19 @@ int main() {
 	cv::namedWindow("resize", cv::WINDOW_AUTOSIZE);
 	cv::namedWindow("crop", cv::WINDOW_AUTOSIZE);
 
+	cv::Mat resize;
+	
 	cv::Mat base = cv::imread("Images/bert en ernie.jpg", cv::IMREAD_COLOR);
-	cv::Mat resize = base.clone();
-	cv::Mat crop = base.clone();
-
-	resize.resize(200);
+	cv::Mat crop= base(cv::Rect(25, 25, 200, base.rows - 50));
+	cv::resize(base, resize, cv::Size(400, 100));
 
 	imshow("base", base);
 	imshow("resize", resize);
 	imshow("crop", crop);
 
 	cv::waitKey(0);
+
+	cv::destroyAllWindows();
+
 	return 0;
 }
