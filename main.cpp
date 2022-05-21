0a042bb4c71bac636389689e48b4d81b6f448fb7
ThunderSphun <manderijn2000@gmail.com>
2022-05-21 12:16:11 +0200

    updated solution

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-int main() {
-	cv::namedWindow("base", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("resize", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("crop", cv::WINDOW_AUTOSIZE);
+using namespace cv;
 
-	cv::Mat resize;
+int main() {
+	namedWindow("base", WINDOW_AUTOSIZE);
+	namedWindow("resize", WINDOW_AUTOSIZE);
+	namedWindow("crop", WINDOW_AUTOSIZE);
+
+	Mat resize;
 	
-	cv::Mat base = cv::imread("Images/bert en ernie.jpg", cv::IMREAD_COLOR);
-	cv::Mat crop= base(cv::Rect(25, 25, 200, base.rows - 50));
-	cv::resize(base, resize, cv::Size(400, 100));
+	Mat base = imread("Images/bert en ernie.jpg", IMREAD_COLOR);
+	Mat crop= base(Rect(25, 25, 200, base.rows - 50));
+	cv::resize(base, resize, Size(400, 100));
 
 	imshow("base", base);
 	imshow("resize", resize);
 	imshow("crop", crop);
 
-	cv::waitKey(0);
+	waitKey(0);
 
-	cv::destroyAllWindows();
+	destroyAllWindows();
 
 	return 0;
 }
