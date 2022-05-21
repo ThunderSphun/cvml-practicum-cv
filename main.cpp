ab99c7db937eee27e2f2cddfe8a852213c1b2a9a
ThunderSphun <manderijn2000@gmail.com>
2022-05-21 17:17:30 +0200

    solution

--- main.cpp
+++ main.cpp
@@ -4,9 +4,33 @@
 using namespace cv;
 
 int main() {
-	namedWindow("template", WINDOW_AUTOSIZE);
+	Mat img = imread("Images/heinz.jpg");
+	Mat gray;
+	Mat canny;
 
-	waitKey(0);
-	destroyAllWindows();
-	return 0;
+	cvtColor(img, gray, COLOR_BGR2GRAY);
+
+	int minThreshold = 0;
+	int maxThreshold = 255;
+
+	namedWindow("Trackbars", WINDOW_AUTOSIZE);
+	namedWindow("original", WINDOW_AUTOSIZE);
+	namedWindow("gray", WINDOW_AUTOSIZE);
+	namedWindow("with canny", WINDOW_AUTOSIZE);
+
+	createTrackbar("minimum threshold", "Trackbars", &minThreshold, 512);
+	createTrackbar("maximum threshold", "Trackbars", &maxThreshold, 512);
+
+	while (true) {
+		Canny(gray, canny, minThreshold, maxThreshold);
+
+		imshow("original", img);
+		imshow("gray", gray);
+		imshow("with canny", canny);
+
+		if (waitKey(1) == 27) {
+			destroyAllWindows();
+			return 0;
+		}
+	}
 }
